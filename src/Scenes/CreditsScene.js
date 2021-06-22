436ba98f7941dc6e02dd473dc0d00150b27c27cc
FrederickMih <fredtimermih@gmail.com>
2021-06-22 15:58:24 +0100

    add CreditScene class

---
+++ src/Scenes/CreditsScene.js
@@ -0,0 +1,11 @@
+import 'phaser';
+
+export default class CreditsScene extends Phaser.Scene {//eslint-disable-line
+  constructor() {
+    super('Credits');
+  }
+
+  //   preload() {}
+
+//   create() {}
+}
