da236b0cd1c6a4e1886e06f74c153f941cc36b5b
FrederickMih <fredtimermih@gmail.com>
2021-06-22 15:51:53 +0100

    Add TitleScene class

---
+++ src/Scenes/TitleScene.js
@@ -0,0 +1,11 @@
+import 'phaser';
+
+export default class TitleScene extends Phaser.Scene {//eslint-disable-line
+  constructor() {
+    super('Title');
+  }
+
+  //   preload() {}
+
+//   create() {}
+}
