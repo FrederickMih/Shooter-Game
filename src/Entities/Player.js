f8ca8d1298130f8dc0217a174d73fa5496c0ba3d
FrederickMih <fredtimermih@gmail.com>
2021-06-23 19:49:42 +0100

    Add Player class and its moves directions

--- src/Entities/Player.js
+++ src/Entities/Player.js
@@ -1,2 +1,31 @@
 import 'phaser';
 import Entity from './Entity';
+
+class Player extends Entity {
+  constructor(scene, x, y, key) {
+    super(scene, x, y, key, 'player');
+    this.setData('speed', 200);
+    this.play('sprPlayer.png');
+    this.setData('isShooting', false);
+    this.setData('timerShootDelay', 10);
+    this.setData('timerShootTick', this.getData('timerShootDelay') - 1);
+    this.setData('score', 0);
+  }
+
+  moveUp() {
+    this.body.velocity.y = -this.getData('speed');
+  }
+
+  moveDown() {
+    this.body.velocity.y = this.getData('speed');
+  }
+
+  moveLeft() {
+    this.body.velocity.x = -this.getData('speed');
+  }
+
+  moveRight() {
+    this.body.velocity.x = this.getData('speed');
+  }
+}
+export default Player;
