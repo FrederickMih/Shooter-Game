16311074d3b1b46117ccf68de5e253c712c4c8b6
FrederickMih <fredtimermih@gmail.com>
2021-07-04 14:42:23 +0100

    Add tests for PlayerNameUtil.js file

---
+++ src/test/util.test.js
@@ -0,0 +1,20 @@
+import { setName, getName } from '../util/PlayerNameUtil';
+
+test("If name input field is empty name should be 'unnamed'", () => {
+  setName('');
+  expect(getName()).toBe('unnamed');
+});
+
+test('name must be a string', () => {
+  expect(typeof getName()).toBe('string');
+});
+
+test('set player name', () => {
+  setName('ilie');
+  expect(getName()).toBe('ilie');
+});
+
+test('set a wrong player name', () => {
+  setName(333);
+  expect(getName()).toBe('unnamed');
+});
